34e876daa995a9a4f86498dcc272525fc7e0a009
Piotr Larkowski <p.larkowski90@gmail.com>
2024-03-23 17:41:40 +0100

    Add two more methods bonusMultiplayer() and bonusForProductsSold by using ternary operations

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
         System.out.println(salaryCalculator.salaryMultiplier(4));
         System.out.println(salaryCalculator.salaryMultiplier(5));
         System.out.println(salaryCalculator.salaryMultiplier(6));
+        System.out.println();
+        System.out.println(salaryCalculator.bonusMultiplier(19));
+        System.out.println(salaryCalculator.bonusMultiplier(20));
+        System.out.println(salaryCalculator.bonusMultiplier(21));
+        System.out.println(salaryCalculator.bonusForProductsSold(5));
         System.out.println("Hello world!");
     }
 }
--- src/SalaryCalculator.java
+++ src/SalaryCalculator.java
@@ -2,4 +2,10 @@ public class SalaryCalculator {
     public double salaryMultiplier(int daysSkipped) {
         return daysSkipped>=5?0.85:1;
     }
+    public int bonusMultiplier(int productsSold) {
+        return productsSold>=20?13:10;
+    }
+    public double bonusForProductsSold(int productsSold) {
+        return productsSold<=20?productsSold*10:productsSold*13;
+    }
 }
